7052df3348110a7e2363ba11360d5b07eaeb8a93
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-07-12 13:16:41 -0300

    menu select openbox and i3

--- ob-arch/00-system-packages.sh
+++ ob-arch/00-system-packages.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+MENU="
+1 - openbox
+2 - i3
+3 - both
+"
+echo $MENU
+read -p "Option: " OPTION
+
+case "$OPTION" in 
+  1)
+    echo "install openbox"
+    sudo pacman -S openbox obconf
+    ;;
+  2)
+    echo "install i3"
+    sudo pacman -S i3-gaps
+    ;;
+  3)
+    echo "install openbox and i3"
+    sudo pacman -S openbox obconf i3-gaps lightdm lightdm-gtk-greeter
+    ;;
+  *)
+    echo "invalid value"
+    exit 1
+    ;;
+  esac
+  
 sudo pacman -S  --needed base-devel
                 curl \
                 xorg-server \
                 xorg-xrandr
-                openbox \
-                obconf \
                 lxappearance \
                 rxvt-unicode \
                 wmctrl \
