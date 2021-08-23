b07876974dbaa90b385245d5553c31581c5932b4
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-08-23 20:07:44 -0300

    remove db install

--- ob-deb/04-arduino-CLI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir ~/Documents/Arduino-CLI
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=~/Documents/Arduino-CLI
--- ob-deb/05-ESP-IDF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install git wget flex bison gperf python3 python3-pip python3-setuptools \
-                     cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
-
-mkdir -p ~/esp && 
-git clone --recursive https://github.com/espressif/esp-idf.git ~/esp
-exec ~/esp/esp-idf/install.sh
-echo alias get_idf='. $HOME/esp/esp-idf/export.sh' >> ~/.bashrc
