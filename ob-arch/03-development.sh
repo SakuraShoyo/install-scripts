a2b6b964eb4e5818e0908306d4c60f9439f45e4d
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-09-26 12:33:01 -0300

    fonts install

--- ob-arch/03-development.sh
+++ ob-arch/03-development.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ## NVIM AND NODE
 echo "Install nvim and node..."
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 sudo pacman -S nodejs-lts-fermium neovim ccls 
 
 ## PYENV
--- ob-arch/06-micelanies.sh
+++ ob-arch/06-micelanies.sh
@@ -2,6 +2,10 @@
 
 ## fonts
 sudo pacman -S ttf-dejavu ttf-liberation noto-fonts powerline-fonts
+mkdir -p Programs && mkdir -p ~/.local/share/fonts 
+git clone git@github.com:Shoyo-0kbps/dotfiles.git ~/Programs
+tar xvpf ~/Programs/dotfiles/fonts/fonts.tar.gz -C ~/.loca/share/fonts/ 
+
 
 ## fzf and cool stufs 
-sudo pacman -S fzf ripgrep fd the_silver_searcher 
+sudo pacman -S fzf 
