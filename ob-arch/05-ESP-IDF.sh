5354d1f5b3dd4e766bb089e677f77707de55dec0
Shoyo-0kbps <mizzaelsantos1@gmail.com>
2021-10-11 12:54:57 -0300

    new tools to development

--- ob-arch/05-ESP-IDF.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo pacman -S gcc make flex bison gperf python-pip cmake ninja ccache dfu-util libusb
-
-mkdir -p ~/esp/esp-idf && 
-git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
-exec ~/esp/esp-idf/install.sh
-echo alias get_idf='. $HOME/esp/esp-idf/export.sh' >> ~/.bashrc
