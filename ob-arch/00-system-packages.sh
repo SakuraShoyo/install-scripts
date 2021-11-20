c1179f4f6cf885fb38ae4b566c7cac47f60a2a51
Shoyo-0kbps <mizzaelsantos1@gmail.com>
2021-11-20 20:05:29 -0300

    added graphic instercafe instalation

--- ob-arch/00-system-packages.sh
+++ ob-arch/00-system-packages.sh
@@ -1,56 +1,7 @@
 #!/bin/bash
 set -e
 mkdir -pv ~/Programs
-git clone https://github.com/Shoyo-0kbps/dotfiles ~/Programs
-
-MENU="
-1 - openbox
-2 - bspwm
-3 - dwm
-4 - both
-"
-echo $MENU
-read -p "Option: " OPTION
-
-case "$OPTION" in 
-  1)
-    echo "install openbox"
-    sudo pacman -S openbox obconf
-    ;;
-  2)
-    echo "install bspwm"
-    sudo pacman -S bspwm sxhkd
-    ;;
-  3)
-    echo "install dwm"
-    wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz -P ~/Programs
-    
-    pushd ~/Programs
-      tar xvf dwm-6.2.tar.gz 
-      cd dwm*/
-      cp ~/Programs/dotfiles/sucklesstools/DWM/* .
-      make && sudo make clean install
-    popd
-
-  4)
-    echo "install openbox and bspwm"
-    sudo pacman -S openbox obconf bspwm sxhkd dmenu lightdm lightdm-gtk-greeter
-    ;;
-  *)
-    echo "invalid value"
-    exit 1
-    ;;
-  esac
   
-
-wget https://dl.suckless.org/tools/dmenu-5.0.tar.gz -P ~/Programs    
-pushd ~/Programs
-    tar xvf dmenu-5.0.tar.gz 
-    cd dmenu*/
-    cp ~/Programs/dotfiles/sucklesstools/dmenu/* .
-    make && sudo make clean install
-popd
-
 sudo pacman -S  --needed base-devel \
                 curl \
                 wget \
@@ -61,7 +12,6 @@ sudo pacman -S  --needed base-devel \
                 wmctrl \
                 xdotool \
                 nitrogen \
-                rofi \
                 pulseaudio \
                 pavucontrol \
                 ranger \
@@ -70,5 +20,5 @@ sudo pacman -S  --needed base-devel \
                 inetutils \
                 xdg-utils \
                 ueberzug \
-                xdpyinfo \
                 xcompmgr
+
